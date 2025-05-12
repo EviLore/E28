86f137f366ee0a2c18e44760492cc66740e0abe3
evilore <evilore@gmail.com>
2025-05-12 15:18:40 -0400

    vercel fix

--- history/vite.config.js
+++ history/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This ensures that the server redirects any 404 responses back to index.html
+    // allowing React Router to handle the routing on the client side
+    historyApiFallback: true,
+  },
 })
